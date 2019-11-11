4a2b83c56248dbda853cbd054e39e3a5ef99ee3a
chansmile2011 <chan.smile2011@gmail.com>
2019-11-11 13:47:39 +0530

    commit done

---
+++ src/main/java/Pom_test/org/testers/Testing1.java
@@ -0,0 +1,18 @@
+package Pom_test.org.testers;
+
+import java.time.LocalDateTime;
+
+import org.testng.annotations.Test;
+
+public class Testing1 {
+	
+	@Test
+	void Tried()
+	{
+		LocalDateTime d = LocalDateTime.now();
+		System.out.println(d);
+	}
+		
+	
+
+}
