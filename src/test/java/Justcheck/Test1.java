218dbca32f31d94f1cb878c90f17a96f36d10a67
chansmile2011 <chan.smile2011@gmail.com>
2019-12-25 18:53:45 +0530

    commited

---
+++ src/test/java/Justcheck/Test1.java
@@ -0,0 +1,18 @@
+package Justcheck;
+
+import java.time.LocalDateTime;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	void Tried()
+	{
+		LocalDateTime d = LocalDateTime.now();
+		System.out.println(d);
+		
+		System.out.println("Hello How are You !!!!!!!!!!!!!");
+	}
+		
+}
